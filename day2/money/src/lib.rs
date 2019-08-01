2aad85673bf4e430e73129a2344436e910ce087b
Nelson Lin <nelson@linyencheng@gmail.com>
2019-08-01 12:07:21 +0800

    generic structs

--- day2/money/src/lib.rs
+++ day2/money/src/lib.rs
@@ -1,8 +1,8 @@
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct USD (i32);
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct CAD (i32);
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct GBP (i32);
 
 pub trait ToUSD {
@@ -23,8 +23,18 @@ pub trait ToUSDv<F> {
 pub trait FromUSDv<F> {
     fn from_uv(&self, _:f32) -> F;
 }
+#[derive(PartialEq,Debug)]
+pub struct Transaction<A> {
+    from_id : i32,
+    to_id : i32,
+    amount : A,
+}
 
+pub trait Account {
+    fn id(&self) -> i32;
+}
 pub struct Ex {
+    ac_id : i32,
     cad : f32,   // conversion rate
     gbp : f32,
 }
@@ -52,17 +62,37 @@ impl FromUSDv<CAD> for Ex {
     }
 }
 
+impl Account for Ex {
+    fn id(&self) -> i32 {
+        self.ac_id
+    }
+}
 pub trait Exchange<F,T> {
     fn convert(&self, _:F) -> T;
 }
 
 impl<E,F,T> Exchange<F,T> for E
-where E:ToUSDv<F>+FromUSDv<T> {
+    where E:ToUSDv<F>+FromUSDv<T> {
     fn convert(&self, f:F) -> T {
         self.from_uv(self.to_uv(f))
     }
 }
 
+
+pub trait ExchangeAccount<F,T> {
+    fn exchange(&self, _:i32, _:i32, _:F) -> (Transaction<F>,Transaction<T>);
+}
+
+impl<E,F,T> ExchangeAccount<F,T> for E 
+    where E:Exchange<F,T> + Account,
+          F:Clone {
+
+    fn exchange(&self, f_id:i32, t_id:i32, amount:F) -> (Transaction<F>,Transaction<T>) {
+        let ft = Transaction{from_id : f_id, to_id:self.id(), amount: amount.clone()};// from transaction
+        let tt = Transaction{from_id : self.id(), to_id: t_id, amount:self.convert(amount)};// to transaction
+        (ft,tt)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,12 +113,20 @@ mod tests {
     #[test]
     fn generic_parameters() {
         let g = GBP(200);
-        let ex = Ex{cad:0.7, gbp:1.3};
+        let ex = Ex{ac_id:0, cad:0.7, gbp:1.3};
         let c = ex.from_uv(ex.to_uv(g));
         assert_eq!(c, CAD(371));
 
         let g2 = GBP(200);
         let c2:CAD = ex.convert(g2);
         assert_eq!(c2, CAD(371));
+    }
+
+    #[test]
+    fn generic_structs() {
+        let ex = Ex{ac_id:30, cad:0.7, gbp:1.3};
+        let (ft,tt) = ex.exchange(20,40,GBP(200));
+        assert_eq!(ft, Transaction{from_id:20, to_id:30, amount:GBP(200)});
+        assert_eq!(tt, Transaction{from_id:30, to_id:40, amount:CAD(371)});
     }
 }
